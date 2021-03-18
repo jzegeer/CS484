d2d2afc63efc141a1c51a8897e27c3e978390841
Anh N <anguye@masonlive.gmu.edu>
2021-03-18 01:07:49 -0400

    Starter code
    Added matplotlib and numpy packages. Added some starter code to play with pandas.

--- house.py
+++ house.py
@@ -1,1 +1,14 @@
+## CS 484 - Project
+## March 2021
+## Anh Nguyen & Jake Zegeer
+
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+## Read train data to a table
+train_data = pd.read_csv('train.csv')
+
+## To view: Right Click -> Run Current File in Interactive Window
+## Display first five rows and their 81 columns
+train_data.head(5)
