2bcf372a1d6659d28d0d6e1a3621b72f2b59a690
Jake <51935523+jzegeer@users.noreply.github.com>
2021-04-12 23:05:27 -0400

    Target value determined

--- santander.py
+++ santander.py
@@ -5,10 +5,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 ## Read train data to a table
 train_data = pd.read_csv('train.csv')
 
+## Read test data to a table
+test_data = pd.read_csv('test.csv')
+
 ## To view: Right Click -> Run Current File in Interactive Window
 ## Display first five rows and their 81 columns
-train_data.head(5)
+print(train_data.head())
+
+#Creates a main target value 
+print(train_data.groupby('target').size())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
